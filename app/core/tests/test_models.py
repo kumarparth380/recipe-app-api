e0765048a26ccbc8aee77e4678def6d68cdd9f82
kumarparth123 <itsaryan380@gmail.com>
2021-07-05 08:21:37 +0530

    add custom user model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -22,8 +22,18 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
-    
+
     def test_new_user_invalid_email(self):
         """Test creating a user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test@123')
+
+    def test_create_new_superuser(self):
+        """Testing creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@yopmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
